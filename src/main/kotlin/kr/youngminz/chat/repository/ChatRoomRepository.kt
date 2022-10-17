1c7e5993584b3585a18934d98c9b0b1db58fffd4
Youngmin Koo <youngminz.kr@gmail.com>
2022-10-18 05:09:29 +0900

    멀티쓰레드 강화

--- src/main/kotlin/kr/youngminz/chat/repository/ChatRoomRepository.kt
+++ src/main/kotlin/kr/youngminz/chat/repository/ChatRoomRepository.kt
@@ -2,11 +2,12 @@ package kr.youngminz.chat.repository
 
 import kr.youngminz.chat.dto.ChatRoomDto
 import org.springframework.stereotype.Repository
+import java.util.concurrent.ConcurrentHashMap
 
 @Repository
 class ChatRoomRepository {
 
-    private val chatRoomDtoMap = mutableMapOf<String, ChatRoomDto>()
+    private val chatRoomDtoMap = ConcurrentHashMap<String, ChatRoomDto>()
 
     fun findAllRooms(): List<ChatRoomDto> {
         return chatRoomDtoMap.values.reversed()
@@ -17,8 +18,6 @@ class ChatRoomRepository {
     }
 
     fun createChatRoomDto(name: String): ChatRoomDto {
-        val room = ChatRoomDto(name)
-        chatRoomDtoMap[room.roomId] = room
-        return room
+        return chatRoomDtoMap.computeIfAbsent(name) { ChatRoomDto(it) }
     }
 }
