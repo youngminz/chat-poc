4ed79a2c2faa6d549c4357f6e0668e376fb817cf
Youngmin Koo <youngminz.kr@gmail.com>
2022-10-18 05:02:16 +0900

    fix

--- src/main/kotlin/kr/youngminz/chat/handler/RedisSubscriber.kt
+++ src/main/kotlin/kr/youngminz/chat/handler/RedisSubscriber.kt
@@ -2,12 +2,12 @@ package kr.youngminz.chat.handler
 
 import org.springframework.data.redis.connection.Message
 import org.springframework.data.redis.connection.MessageListener
-import org.springframework.messaging.simp.SimpMessageSendingOperations
+import org.springframework.messaging.simp.SimpMessagingTemplate
 import org.springframework.stereotype.Service
 
 @Service
 class RedisSubscriber(
-    private val messagingTemplate: SimpMessageSendingOperations,
+    private val messagingTemplate: SimpMessagingTemplate,
 ) : MessageListener {
 
     override fun onMessage(message: Message, pattern: ByteArray?) {
